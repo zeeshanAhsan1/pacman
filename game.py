ba4ff0f20259d46a81a5a2022e60d792c7b1f92d
Zeeshan Ahsan <zeeshan.ahsan15@gmail.com>
2024-07-19 01:22:19 -0400

    draw_board func to draw the game board

--- game.py
+++ game.py
@@ -1,21 +1,64 @@
 import pygame
-
+from pygame.locals import *
+from board import boards
+import copy
+import math
 pygame.init()
+
 
 WIDTH = 700
 HEIGHT = 750
-
+color = 'blue'
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
 timer = pygame.time.Clock()
 fps = 60
-
+PI = math.pi
 font = pygame.font.Font('Helvetica.ttc', 20)
+level = copy.deepcopy(boards)
+
+
+def draw_board():
+    # Size of each tile on the board
+    num1 = (HEIGHT - 50) // 32   #Here 50 padding at bottom so remove 50 and 32 rows
+    num2 = WIDTH // 30   #30 Cols
+    
+    for i in range(len(level)):
+        for j in range(len(level[i])):
+            if level[i][j] == 1:
+                pygame.draw.circle(screen, 'white', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 4)
+            if level[i][j] == 2:
+                pygame.draw.circle(screen, 'white', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 10)
+            if level[i][j] == 3:
+                pygame.draw.line(screen, color, (j * num2 + (0.5 * num2), i * num1),
+                                 (j * num2 + (0.5 * num2), i * num1 + num1), 3)
+            if level[i][j] == 4:
+                pygame.draw.line(screen, color, (j * num2, i * num1 + (0.5 * num1)),
+                                 (j * num2 + num2, i * num1 + (0.5 * num1)), 3)
+            if level[i][j] == 5:
+                pygame.draw.arc(screen, color, [(j * num2 - (num2 * 0.4)) - 2, (i * num1 + (0.5 * num1)), num2, num1],
+                                0, PI / 2, 3)
+            if level[i][j] == 6:
+                pygame.draw.arc(screen, color,
+                                [(j * num2 + (num2 * 0.5)), (i * num1 + (0.5 * num1)), num2, num1], PI / 2, PI, 3)
+            if level[i][j] == 7:
+                pygame.draw.arc(screen, color, [(j * num2 + (num2 * 0.5)), (i * num1 - (0.4 * num1)), num2, num1], PI,
+                                3 * PI / 2, 3)
+            if level[i][j] == 8:
+                pygame.draw.arc(screen, color,
+                                [(j * num2 - (num2 * 0.4)) - 2, (i * num1 - (0.4 * num1)), num2, num1], 3 * PI / 2,
+                                2 * PI, 3)
+            if level[i][j] == 9:
+                pygame.draw.line(screen, 'white', (j * num2, i * num1 + (0.5 * num1)),
+                                 (j * num2 + num2, i * num1 + (0.5 * num1)), 3)
+            
+
 
 run = True
-
 while run:
     timer.tick(fps)
     screen.fill('black')
+    draw_board()
+    pygame.display.flip()   #Very important to update display buffer
     
     
     for event in pygame.event.get():
